7a4e1c64d88081f887517e811f240b2356d1f4a8
Gustavo Marques <gustavo@trackmob.com.br>
2020-03-26 08:24:23 -0300

    Add IncidentController, OngController, ProfileController

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,18 @@
 const express = require('express')
-const crypto = require('crypto')
 
-const connection = require('./database/connection')
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 const routes = express.Router()
 
-routes.get('/ongs', async (request, response) => {
-  const ongs = await connection('ongs').select('*');
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
-  return response.json({ ongs })
-})
+routes.post('/incidents', IncidentController.create)
+routes.get('/incidents', IncidentController.index)
+routes.delete('/incidents/:id', IncidentController.delete)
 
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id, name, email, whatsapp, city, uf
-  })
-
-  return response.json({ id })
-})
+routes.get('/profile', ProfileController.index)
 
 module.exports = routes
