00913d5581be2f6eb10afc940f93d4eabbf997f2
Gustavo Marques <gustavo@trackmob.com.br>
2020-03-25 21:31:08 -0300

    app base configuration

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const crypto = require('crypto')
 
-const routes = express.Router();
+const connection = require('./database/connection')
 
-routes.post('/', (request, response) => {
-  return response.json({
-    it: 'works'
+const routes = express.Router()
+
+routes.get('/ongs', async (request, response) => {
+  const ongs = await connection('ongs').select('*');
+
+  return response.json({ ongs })
+})
+
+routes.post('/ongs', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id, name, email, whatsapp, city, uf
   })
+
+  return response.json({ id })
 })
 
 module.exports = routes
