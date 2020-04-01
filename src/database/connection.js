6d147dcdd5bb23cf41791b09b97bf21a3758180a
Gustavo Marques <gustavoufms@gmail.com>
2020-04-01 09:25:34 -0300

    create connection for test environment

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex')
 const configuration = require('../../knexfile')
 
+const environment = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
 const connection = knex(configuration.development)
 
 module.exports = connection
--- src/routes.js
+++ src/routes.js
@@ -14,8 +14,9 @@ routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(10).max(11),
-    city: Joi.string().required().length(2)
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
   })
 }),OngController.create)
 
