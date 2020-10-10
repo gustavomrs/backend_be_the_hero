8eea8839ccbc124d1f0ecb82b32b9a19e1a648bf
Gustavo Marques <gustavoufms@gmail.com>
2020-10-10 13:31:04 -0300

    x-total-count header

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -19,7 +19,7 @@ module.exports = {
         'ongs.uf',
       ]);
 
-    response.header('X-Total-Count', count['count(*)'])
+    await response.header('X-Total-Count', count['count(*)'])
 
     return response.json({ incidents })
   },
